07a23bfe67080435818d968571b0bf8431eaccb4
Aleksandr Kalinin <kalinin.aleksandr.v@gmail.com>
2022-09-08 19:36:46 +0300

    initial

---
+++ src/main/java/com/github/kalininaleksandrv/MakespringgreatagainApplication.java
@@ -0,0 +1,13 @@
+package com.github.kalininaleksandrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakespringgreatagainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakespringgreatagainApplication.class, args);
+	}
+
+}
