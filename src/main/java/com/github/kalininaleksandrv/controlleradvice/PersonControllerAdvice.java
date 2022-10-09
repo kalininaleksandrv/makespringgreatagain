09c16239de895aae998b37f431dd838c0e90182a
Aleksandr Kalinin <kalinin.aleksandr.v@gmail.com>
2022-10-09 11:26:01 +0300

    add custom controller advice to wrap result

--- src/main/java/com/github/kalininaleksandrv/controlleradvice/PersonControllerAdvice.java
+++ src/main/java/com/github/kalininaleksandrv/controlleradvice/PersonControllerAdvice.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 works only if class marked as a Version1
  */
 @ControllerAdvice(annotations = Version1.class)
-public class PersonControllerAdvice implements ResponseBodyAdvice {
+public class PersonControllerAdvice implements ResponseBodyAdvice<Object> {
 
     @Override
     public boolean supports(MethodParameter returnType, Class converterType) {
@@ -20,7 +20,12 @@ public class PersonControllerAdvice implements ResponseBodyAdvice {
     }
 
     @Override
-    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+    public Object beforeBodyWrite(Object body,
+                                  MethodParameter returnType,
+                                  MediaType selectedContentType,
+                                  Class selectedConverterType,
+                                  ServerHttpRequest request,
+                                  ServerHttpResponse response) {
         return new PersonWrapper((Person) body, true);
     }
 }
