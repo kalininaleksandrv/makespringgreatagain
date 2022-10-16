ee22ad32083ba2ffbd2b5f5aae876406a60b76b2
Aleksandr Kalinin <kalinin.aleksandr.v@gmail.com>
2022-10-16 16:25:11 +0300

    add method annotation to disable wrapper

--- src/main/java/com/github/kalininaleksandrv/controlleradvice/PersonController.java
+++ src/main/java/com/github/kalininaleksandrv/controlleradvice/PersonController.java
@@ -6,16 +6,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @Version1
 public class PersonController {
 
     @GetMapping(value = "/persons/{id}")
     ResponseEntity<Person> getPerson(@PathVariable Long id) {
-        if(id == 0) throw new RuntimeException();
+        if (id == 0) throw new RuntimeException();
         if (id == 1) {
             return new ResponseEntity<>(new Person(1, "John Doe"), HttpStatus.OK);
         }
         throw new PersonProcessingException("no such person");
+    }
+
+    @DisableWrapper
+    @GetMapping(value = "/persons")
+    ResponseEntity<List<Person>> getAllPersons() {
+        return new ResponseEntity<>(List.of(new Person(1, "John"), new Person(2, "Jane")), HttpStatus.OK);
     }
 }
--- src/main/java/com/github/kalininaleksandrv/controlleradvice/PersonControllerAdvice.java
+++ src/main/java/com/github/kalininaleksandrv/controlleradvice/PersonControllerAdvice.java
@@ -2,11 +2,12 @@ package com.github.kalininaleksandrv.controlleradvice;
 
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import java.lang.annotation.Annotation;
 
 /*
 works only if class marked as a Version1
@@ -16,6 +17,13 @@ public class PersonControllerAdvice implements ResponseBodyAdvice<Object> {
 
     @Override
     public boolean supports(MethodParameter returnType, Class converterType) {
+
+        //if we find @DisableWrapper above method - advice will be disabled
+        for (Annotation a: returnType.getMethodAnnotations()) {
+            if (a.annotationType() == DisableWrapper.class) {
+                return false;
+            }
+        }
         return true;
     }
 
